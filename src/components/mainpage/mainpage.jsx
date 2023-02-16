9c99bffec8709c52ccaddb533ed9031f4bcd9671
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-02-16 19:16:30 +0530

    made my first page of my protofolio

--- src/components/mainpage/mainpage.jsx
+++ src/components/mainpage/mainpage.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./mainpage.css";
-import myimage from "../images/photo2.png";
+import myimage from "../images/photo.png";
+import crystal from "../images/crystal.png";
 
 const Mainpage = () => {
   return (
     <div className="Mainpage-container">
+      <img src={crystal} />
       <div className="main-left">
         <img src={myimage} alt="myimage" />
       </div>
-      <div className="main-right"></div>
+      <div className="main-right">
+        <h1>Who is GANESH?</h1>
+        <p>
+          <span>Great to meet you!</span>
+          <br />I am an engineering student who's really interested in web
+          development. I like to work hard and smart to achieve my goals. I am
+          currently 21 years old and eager to find real world problems in the
+          web development field so you can demonstrate my skills and abilities.
+        </p>
+        <p>Loves to learn new things in my field</p>
+        <button>MORE</button>
+      </div>
     </div>
   );
 };
