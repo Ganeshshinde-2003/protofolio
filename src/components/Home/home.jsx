a45de36cca592cc29217d394cf94fda53876d14b
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-02-18 12:10:10 +0530

    day 3 part 2

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import { GoThreeBars } from "react-icons/go";
 import { FaTimes } from "react-icons/fa";
+import bitemoji from "../images/bitemoji2.png";
 import "./home.css";
 
 const Home = () => {
+  const list = [
+    "Web Developer",
+    "Front-End Developer",
+    "Back-End Developer",
+    "Coder",
+  ];
   const [show, setShow] = useState(true);
   const [menu, setmenu] = useState(true);
+  const [me, setMe] = useState(list[0]);
+  let i = 0;
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      if (i !== 4) {
+        setMe(list[i]);
+        i = i + 1;
+      } else {
+        i = 0;
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
   const toggleMenu = () => {
     setShow((show) => !show);
     setmenu(!menu);
@@ -45,8 +68,21 @@ const Home = () => {
       </header>
       <div className="main">
         <div className="hero-section-one">
-          <div className="text-part"></div>
-          <div className="image-part"></div>
+          <div className="text-part">
+            <h1>Hey!! It's Ganesh</h1>
+            <h2>I am a {me}</h2>
+            <p>
+              I’m a software developer and here is my portfolio website. Here
+              you’ll learn about my journey as a software developer.
+            </p>
+          </div>
+          <div className="image-part">
+            <h1 className="name">
+              <span className="firstname">Ganesh</span>
+              <br /> <span className="lastname">Shinde</span>
+            </h1>
+            <img className="imageofmine" src={bitemoji} />
+          </div>
         </div>
       </div>
     </div>
