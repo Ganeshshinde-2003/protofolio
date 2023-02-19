8c0a2aa0d7a70568f060bb8d7a2921f98d974129
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-02-19 14:03:34 +0530

    day 4 added footer section

--- src/components/Home/contact.jsx
+++ src/components/Home/contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./contact.css";
+import { FaGithub, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -24,6 +26,38 @@ const Contact = () => {
             value="Send Message"
           />
         </form>
+      </div>
+      <div className="footer">
+        <h1>Ganesh Shinde</h1>
+        <div className="footer-container">
+          <div className="left-footer">
+            <h3>Designed By @Ganesh Shinde</h3>
+            <h5>@All Rights are Reserved</h5>
+          </div>
+          <div className="right-footer">
+            <Link
+              to="https://github.com/Ganeshshinde-2003"
+              className="footer-icons"
+            >
+              <FaGithub />
+            </Link>
+            <Link
+              to="https://www.instagram.com/gani_shinde_/"
+              className="footer-icons"
+            >
+              <FaInstagram />
+            </Link>
+            <Link to="https://twitter.com/Gani_Shinde" className="footer-icons">
+              <FaTwitter />
+            </Link>
+            <Link
+              to="https://www.linkedin.com/in/ganesh-shinde-01380825b/"
+              className="footer-icons"
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
