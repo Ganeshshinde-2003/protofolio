c66916d3a65938d6cba7c1a4f995deebd06d1913
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-02-19 17:25:59 +0530

    day 4 firebase database is added

--- src/components/Home/contact.jsx
+++ src/components/Home/contact.jsx
@@ -1,24 +1,78 @@
 import React from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "./contact.css";
+import { useForm } from "react-hook-form";
 import { FaGithub, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import firebase from "./firebase.js";
 
 const Contact = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required("You Must Add A Name..."),
+    email: yup.string().required("You Must Add A Email..."),
+    sub: yup.string(),
+    description: yup
+      .string()
+      .required("You Must Add A Description About Your Post"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onCreatMessage = async (data) => {
+    // // const data2 = {
+    // //   title: data.title,
+    // //   email: data.email,
+    // //   sub: data.sub,
+    // //   discription: data.discription,
+    // // };
+    // await addDoc(collection(db, "message"), {
+    //   title: data.title,
+    //   email: data.email,
+    //   sub: data.sub,
+    //   discription: data.discription,
+    // });
+  };
+
   return (
     <div className="contactme">
       <div className="formFill">
-        <form id="formcreate">
+        <form id="formcreate" onSubmit={handleSubmit(onCreatMessage)}>
           <h1 className="creat"> Take A Coffee And Chat With Me </h1>
-          <input id="titlename" placeholder="Enter Name" />
-          <input id="emailname" type="email" placeholder="Enter Your Email" />
-          <input type="text" id="subjectname" placeholder="Enter The Subject" />
+          <input
+            id="titlename"
+            placeholder="Enter Name"
+            {...register("title")}
+          />
+          <p>{errors.title?.message}</p>
+          <input
+            id="emailname"
+            type="email"
+            placeholder="Enter Your Email"
+            {...register("email")}
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            type="text"
+            id="subjectname"
+            placeholder="Enter The Subject"
+            {...register("sub")}
+          />
           <textarea
             id="textareadis"
             placeholder="Description..."
             cols={40}
             rows={10}
             draggable={false}
+            {...register("discription")}
           />
+          <p>{errors.discription?.message}</p>
           <input
             id="submit"
             class="button-57"
--- src/components/Home/firebase.js
+++ src/components/Home/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase";
+import "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +16,6 @@ const firebaseConfig = {
   measurementId: "G-37K1TESFFQ",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
